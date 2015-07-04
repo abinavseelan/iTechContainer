6acf55d20d2b6aab2ef092cb3cfb672cb48c345f
Abinav Seelan <abinav.n.seelan@gmail.com>
2015-07-05 01:03:59 +0530

    Started Work on Auth System

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,62 +18,28 @@
 
 
 angular.module('itcApp')
-.controller('MainCtrl', ['$scope','$mdSidenav', function($scope, $mdSidenav){
-	$scope.openLeftMenu = function() {
-    $mdSidenav('left').toggle();
-  };
-  Parse.initialize("WSpkgtQhqwNA49k2kpuJllJrGyzEJ41hcgXSAIXv", "BzNKo3YGzGeoWEtQUPLlfgZerYNGED9on6DEAfGd");
-  
-  var Temperature = Parse.Object.extend('Temperature');
-  var Pressure = Parse.Object.extend('Pressure');
-  var Humidity = Parse.Object.extend('Humidity');
-  var Moisture = Parse.Object.extend('Moisture');
+.controller('MainCtrl', ['$scope', function($scope){
+    
 
+    Parse.initialize("WSpkgtQhqwNA49k2kpuJllJrGyzEJ41hcgXSAIXv", "BzNKo3YGzGeoWEtQUPLlfgZerYNGED9on6DEAfGd");
 
-  function sendData(){
+    var User = Parse.Object.extend("Users");
 
-    var temperatureValue = $scope.sense.temp;
-    var humidityValue = $scope.sense.hum;
-    var pressureValue = $scope.sense.pres;
-    //var moistureValue = $('#moisture-value').val();
+    $scope.clearData = function(){
+      $scope.username = "";
+      $scope.password = "";
+    };
 
-    var temperature = new Temperature();
-    var humidity = new Humidity();
-    //var moisture = new Moisture();
-    var pressure = new Pressure();
+    $scope.authUser = function(){
+      var users = new Parse.Query(User);
 
+      users.find({success: function(){
+        for(var user in users){
+          
+        }
 
-    humidity.set({value: humidityValue});
-    humidity.save({success: function(){
-
-
-    }, error: function(error){
-      console.log("Error: " + error.message);
-    }});
-
-    /*moisture.set({value: moistureValue});
-    moisture.save({success: function(){
-
-
-    }, error: function(error){
-      console.log("Error: " + error.message);
-    }});*/
-
-    pressure.set({value: pressureValue});
-    pressure.save({success: function(){
-
-
-    }, error: function(error){
-      console.log("Error: " + error.message);
-    }});
-
-    temperature.set({value: temperatureValue});
-    temperature.save({success: function(){
-
-
-    }, error: function(error){
-      console.log("Error: " + error.message);
-    }});
-  };
-  setInterval(sendData,5000);
+      }, error: function(error){
+        console.log("Query Error: "+error.message);
+      }})
+    }
 }]);
